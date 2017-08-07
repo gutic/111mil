9e0eeaafc5f009df7e5f10e0c3ab0c65d8d8be58
agustin <agustinterlaak@gmail.com>
2017-08-07 10:48:42 -0300

    prueba de clases

---
+++ test/src/test/Test2.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ *
+ * @author agustin
+ */
+public class Test2 {
+    String algo;
+    public static void main(String algo) {
+        System.out.print(algo);
+    }
+    
+
+}
