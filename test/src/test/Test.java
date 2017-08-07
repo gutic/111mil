f9c611640dd7ca744ad9b9d050d6fe36af958cd5
agustin <agustinterlaak@gmail.com>
2017-08-07 10:17:16 -0300

    Probando git en netbeans

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -17,37 +17,33 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         Scanner teclado = new Scanner(System.in);
-       float nota = 0;
-       float sumatoria = 0;
-       int cant = 0;
-       int horas = 0;
-//       for (int i = 1; i <= 10; i++) {
-//           System.out.print("ingrese las horas trabajadas \n");
-//           horas = teclado.nextInt();
-//           System.out.println("total a pagar al trabajador " + i + " es =  "  + horas*500);
-//       }
-       do{
-           System.out.print("ingreseasd \n");
-           nota = teclado.nextInt();
-       }while(nota != 0);
-       
-//        System.out.print("ingrese la nota");
-//        nota = teclado.nextInt();
-//        
-//        while(nota != 0){
-//            System.out.print("ingrese la nota");
-//            if (nota >= 1 && nota <= 10){
-//                sumatoria += nota;
-//                cant +=1;
-//                nota = teclado.nextInt();
-//            }
-//            else{
-//                System.out.print("Dato invalido");
-//            }
-//        }
-//        System.out.print("El promedio es  =  " + sumatoria/cant);
+        float nota = 0;
+        float sumatoria = 0;
+        int cant = 0;
+        int horas = 0;
+        /*for (int i = 1; i <= 10; i++) {
+            System.out.print("ingrese las horas trabajadas \n");
+            horas = teclado.nextInt();
+            System.out.println("total a pagar al trabajador " + i + " es =  "  + horas*500);
+        }
+        */   
+         System.out.print("ingrese la nota");
+         nota = teclado.nextInt();
+         
+         while(nota != 0){
+             System.out.print("ingrese la nota");
+             if (nota >= 1 && nota <= 10){
+                sumatoria += nota;
+                cant +=1;
+                nota = teclado.nextInt();
+            }
+            else{
+                System.out.print("Dato invalido");
+            }
+        }
+        System.out.print("El promedio es  =  " + sumatoria/cant);
                 
     }
     
